4e451d422cf63b438d685e4b2ec7946352e5d2b0
claw0ed <claw0ed@gmail.com>
2018-01-02 16:35:40 +0900

    2018.01.02

--- JavaFXProject/src/claw0ed/controller/FXmain4Controller.java
+++ JavaFXProject/src/claw0ed/controller/FXmain4Controller.java
@@ -1,51 +1,13 @@
 package claw0ed.controller;
 
 import javafx.event.ActionEvent;
-import javafx.fxml.FXML;
-import javafx.scene.control.Alert;
-import javafx.scene.control.Label;
-import javafx.scene.control.PasswordField;
-import javafx.scene.control.TextField;
 
 public class FXmain4Controller {
 
-	// FXML에 정의된 컨트롤을 Controller 에서
-	// 제어하기 위해 멤버변수로 정의
-	@FXML TextField uid;
-	@FXML PasswordField pwd;
-	@FXML Label loginok;
-	@FXML Label loginno;
 	
-	// FXML에 정의된 컨트롤의 이벤트를 처리하기 위해
-	// 이벤트 처리 메서드 정의
-	String isLogin = "로그인 실패!!";
-	
-	public void go2login(ActionEvent ae) {
-		if(uid.getText().equals("claw0ed") && pwd.getText().equals("123456")) {
-			System.out.println("로그인 성공!!");
-			isLogin = "로그인 성공!!";
-			loginok.setVisible(true);
-		} else {
-			System.out.println("로그인 실패!!");
-			loginno.setVisible(true);
-		}
-			
-		// 대화상자 만들기
-		Alert dialog = new Alert(Alert.AlertType.WARNING);
-		dialog.setTitle(isLogin);
-		dialog.setHeaderText(isLogin);
-		dialog.setContentText(isLogin);
-		
-		// 대화상자 화면에 표시
-		dialog.showAndWait();
+	public void openAbout(ActionEvent ae) {
 	}
 	
-	public void  go2reset(ActionEvent ae) {
-		// 입력상자의 모든 내용을 지움
-		uid.setText("");
-		pwd.clear();
-		loginok.setVisible(false);
-		loginno.setVisible(false);
-		
+	public void exitApp(ActionEvent ae) {
 	}
 }
--- JavaFXProject/src/claw0ed/main/FXmain4.java
+++ JavaFXProject/src/claw0ed/main/FXmain4.java
@@ -15,7 +15,7 @@ public class FXmain4 extends Application {
 	public void start(Stage pStage) throws IOException {
 		Parent root = FXMLLoader.load(getClass().getResource("/claw0ed/view/FXmain4.fxml"));
 		
-		pStage.setTitle("Border레이아웃,eoghktkdwk,xpdlqmf");
+		pStage.setTitle("Border레이아웃,대화상자,테이블");
 		pStage.getIcons().add(new Image("/claw0ed/icons/twitter.png"));
 		pStage.setScene(new Scene(root, 640, 480));
 		
