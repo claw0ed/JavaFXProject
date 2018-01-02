eb3be369e962f13565508fc6773b5dd5e87d8601
claw0ed <claw0ed@gmail.com>
2018-01-02 14:48:24 +0900

    2018.01.02

---
+++ JavaFXProject/src/claw0ed/controller/FXmain3Controller.java
@@ -0,0 +1,24 @@
+package claw0ed.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.PasswordField;
+import javafx.scene.control.TextField;
+
+public class FXmain3Controller {
+
+	// FXML에 정의된 컨트롤을 Controller 에서
+	// 제어하기 위해 멤버변수로 정의
+	@FXML TextField uid;
+	@FXML PasswordField pwd;
+	
+	// FXML에 정의된 컨트롤의 이벤트를 처리하기 위해
+	// 이벤트 처리 메서드 정의
+	public void go2login(ActionEvent ae) {
+		
+	}
+	
+	public void  go2reset(ActionEvent ae) {
+		
+	}
+}
---
+++ JavaFXProject/src/claw0ed/main/FXmain3.java
@@ -0,0 +1,16 @@
+package claw0ed.main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class FXmain3 extends Application {
+
+	@Override
+	public void start(Stage primaryStage) {
+		
+	}
+
+	public static void main(String[] args) {
+		launch(args);
+	}
+}
